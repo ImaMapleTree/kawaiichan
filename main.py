7572085dee476788beda8d2bb0557b42f9f52d7c
BuildTools <unconfigured@null.spigotmc.org>
2021-09-14 18:03:10 -0400

    Script-Push

--- main.py
+++ main.py
@@ -63,38 +63,14 @@ async def slash_play(ctx, song):
     await backend.play(ctx, song)
 
 @client.command(pass_context=True)
-async def join(ctx):
-    """Joins the same voice channel as the user whom called the command"""
-
-    if ctx.voice_client is not None:
-        return await ctx.voice_client.move_to(ctx.message.author.voice.channel)
-
-    await ctx.message.author.voice.channel.connect()
-
-@client.command(pass_context=True)
 async def source_volume(ctx, volume):
     volume = int(volume)/100
     await backend.source_volume(ctx, volume=volume)
-
-@slash.subcommand(base="volume", **utils.cmd_gen("source_volume"))
-async def slash_source_volume(ctx, volume):
-    await backend.source_volume(ctx, volume=volume)
-
-'''
-@slash.subcommand(base="volume", **utils.cmd_gen("player_volume"))
-async def slash_player_volume(ctx, volume):
-    volume = volume if volume <= 100 else 50
-    volume = volume/100
-    await backend.player_volume(ctx, volume=volume)
-'''
 
 @slash.slash(name="shutdown", description="Please don't use this, genuinely.")
 async def slash_shutdown(ctx):
     await ctx.send("Shutting down!")
     sys.exit(2)
 
-@client.command(pass_context=True)
-async def shutdown(ctx):
-    sys.exit(2)
-
-client.run('MjAwNDgwMTg1MDQ3MzE4NTI4.V33luA.HHJucLwp1FAqiXxX-4-hO3TiabQ')
+#client.run('MjAwNDgwMTg1MDQ3MzE4NTI4.V33luA.HHJucLwp1FAqiXxX-4-hO3TiabQ') #Main bot
+client.run('MjAwOTkyNTc3MTc5MjIyMDE2.V3_C7A.4d4DaKOALJDo4HqANVe6PJDkQl8') #Test bot (Ezreal)
